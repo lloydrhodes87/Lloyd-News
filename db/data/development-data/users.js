6ac99ec37075b2d4b5807cbb9e419cedb4caa6c1
Lloyd Rhodes <lloydrhodes@hotmail.co.uk>
2019-01-24 08:33:09 +0000

    working avatar images added

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -2,31 +2,32 @@ module.exports = [
   {
     username: 'tickle122',
     name: 'Tom Tickle',
-    avatar_url: 'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg',
+    avatar_url: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads02&accessoriesType=Wayfarers&hairColor=Black&facialHairType=Blank&facialHairColor=Blonde&clotheType=BlazerShirt&clotheColor=PastelOrange&graphicType=Diamond&eyeType=Happy&eyebrowType=SadConcerned&mouthType=Concerned&skinColor=Pale',
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
-    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg',
+    avatar_url: 'https://avataaars.io/?avatarStyle=Circle&topType=NoHair&accessoriesType=Prescription01&hairColor=BlondeGolden&facialHairType=BeardMedium&facialHairColor=Platinum&clotheType=Hoodie&clotheColor=Blue01&eyeType=Squint&eyebrowType=UpDownNatural&mouthType=Smile&skinColor=Light',
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
-    avatar_url: 'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+    avatar_url: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraightStrand&accessoriesType=Prescription01&hairColor=Auburn&facialHairType=Blank&clotheType=Hoodie&clotheColor=PastelRed&eyeType=Surprised&eyebrowType=UnibrowNatural&mouthType=Tongue&skinColor=Tanned'
+,
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
-    avatar_url: 'https://i.imgur.com/WfX0Neu.jpg',
+    avatar_url: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=PastelBlue&eyeType=Default&eyebrowType=UpDownNatural&mouthType=Grimace&skinColor=Black',
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
-    avatar_url: 'https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg',
+    avatar_url: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairNotTooLong&accessoriesType=Prescription01&hairColor=Auburn&facialHairType=Blank&clotheType=ShirtScoopNeck&clotheColor=PastelOrange&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Grimace&skinColor=Brown',
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
-    avatar_url: 'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg',
+    avatar_url: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairBob&accessoriesType=Round&hairColor=Black&facialHairType=Blank&facialHairColor=BlondeGolden&clotheType=Overall&clotheColor=Gray02&eyeType=Squint&eyebrowType=RaisedExcitedNatural&mouthType=Eating&skinColor=Light',
   },
 ];
